4ba5f3601ea171553d43ab5c09693732461a1025
atikxcode <liadbd20@gmail.com>
2024-06-08 05:18:52 +0600

    created api based on id to find donation request

--- index.js
+++ index.js
@@ -103,12 +103,7 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/donationrequest/:id', async(req, res) => {
-      const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await donationRequestCollection.findOne(query);
-      res.send(result);
-    })
+   
 
 
 
