3a00f1b040173467d999e1d1587f321da254defb
atikxcode <liadbd20@gmail.com>
2024-06-09 01:29:38 +0600

    blog data loading api created

--- index.js
+++ index.js
@@ -194,7 +194,19 @@ async function run() {
      })
 
 
-   
+     app.get('/blog', async(req, res) => {
+      const cursor = blogCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/blog/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await blogCollection.findOne(query);
+      res.send(result);
+    })
+
 
 
     
